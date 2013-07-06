276c8480ab5e5a177b197d89b0dd70232380645d
Anubhab Baksi <anubhab91@gmail.com>
2013-07-06 20:03:02 +0530

    bug fixed

--- 1 (Matching sub lists).pl
+++ 1 (Matching sub lists).pl
@@ -1,5 +1,6 @@
 %Matching sub lists
 %Note that `accept([0, 1, 0, 0]).` will be proven false.
+
 %%%%%%Output (in SWI Prolog)%%%%%%%%
 %%?- accept([1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0]).
 %%true.
@@ -9,16 +10,14 @@
 %%%%%%%%
 
 accept(List) :- 
-	append(_Left1, [0, 1|_Rest], List), 
-	append(X, [1, 0, 0|_Right2], _Rest), 
-	append(_Right2,Y, _Rest), 
-	(accept(X), accept(Y)),
+	append(_, [0, 1|Rest], List), 
+	append(InBetween, [1, 0, 0|RightMost], Rest), 
+	(accept(InBetween), accept(RightMost)),
 	!.
 
 accept(List) :- 
-	append(_, [0, 1|_Rest], List),
+	append(_, [0, 1|Rest], List),
 	!,
 	fail.
 	
-accept(List) :- 
-	!.
+accept(List):-!.
